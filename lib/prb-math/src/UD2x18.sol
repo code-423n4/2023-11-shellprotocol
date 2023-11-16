4f7eca0d29abcd8a3acdfb09df98f4161b83aea0
viraj124 <virajm72@gmail.com>
2023-11-16 14:56:20 +0300

    chore: remove unused submodules

--- lib/prb-math/src/UD2x18.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.13;
-
-import "./ud2x18/Casting.sol";
-import "./ud2x18/Constants.sol";
-import "./ud2x18/Errors.sol";
-import "./ud2x18/ValueType.sol";
